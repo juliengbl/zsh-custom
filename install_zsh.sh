8db7b57b7c510fa8220b8316c11cf01868fd9c69
Julien GERBEL <69520517+julien-gerbel@users.noreply.github.com>
2022-02-25 10:01:01 +0100

    Delete install_zsh.sh

--- install_zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-cp ./.zshrc ~/
